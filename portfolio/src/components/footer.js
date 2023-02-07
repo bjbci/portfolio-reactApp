590a9e198abe72f2e62b0a097098482e6cb0347f
Brian Bulowicz <bajebi.bb@gmail.com>
2023-02-07 03:14:01 -0600

    styles a little better

--- portfolio/src/components/footer.js
+++ portfolio/src/components/footer.js
@@ -1,55 +1,12 @@
 const Footer= ({view,setVIew})=>{
 
     return (
-//  <nav className="navbar fixed-bottom navbar-expand-lg navbar-light bg-success">
-//     <div className="container">
-//         {/* <a className="navbar-brand" href="#"></a> */}
-//             <ul className="navbar-nav">
-//                  <div className="row">
-//                      <div class="col align-self-center">
-//                         <li className="nav-item">
-//                             <a 
-//                                 className="nav-link"
-//                                 href="#"
-//                         >
-//                             <i className="bi bi-envelope" ></i>
-//                             </a>
-//                         </li>
-//                         </div>
-//                         <div class="col">
-//                         <li className="nav-item">
-//                             <a 
-//                                 className="nav-link"
-//                                 href="#"
-//                         >
-//                         <i className="bi bi-linkedin"></i>
-//                             </a>
-//                         </li>
-//                         </div>
-//                         <div class="col">
-//                         <li className="nav-item">
-//                             <a 
-//                                 className="nav-link" 
-//                                 href="#"
-//                         >
-//                         <i className="bi bi-github"></i>
-//                             </a>
-//                         </li>
-//                     </div>
-//             </div>
-//              </ul>
-//      </div>
-// </nav>
-   
+ <nav className="navbar fixed-bottom navbar-expand-lg navbar-light bg-success">
+  
 
-<div className="row align-items-end">
+
 <div className="col">
-{/* <a 
- className="nav-link" 
- href="#"
->
- <i className="bi bi-github"></i>
- </a> */}
+
 </div>
 <div className="col" style={{
     display:"flex",
@@ -75,14 +32,13 @@ href="#"
  </a>
 </div>
 <div className="col">
-{/* <a 
- className="nav-link"
- href="#"
- >
-<i className="bi bi-envelope" ></i>
- </a> */}
+
 </div>
-</div>
+
+
+</nav>
+   
+
 
 ) 
 
